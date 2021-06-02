db7c4f2f5ccf93504ff79deb5be123342c770fcb
Piyush Agarwal <agpiyush707010@gmail.com>
2021-06-02 13:31:29 +0530

    both functions added

--- estimate.py
+++ estimate.py
@@ -51,5 +51,5 @@ class TestMC(unittest.TestCase):
             self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
         
     
-if _name_ == "_main_":
+
     unittest.main()
